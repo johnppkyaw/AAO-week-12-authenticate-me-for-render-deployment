a626db7e47a8375f341c8caabf5d18deee6eb662
John <jjpyae@gmail.com>
2023-10-24 00:12:30 -0700

    completes phase 2

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -6,6 +6,9 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+//Validation error from sequelize
+const {ValidationError} = require('sequelize');
+
 //check whether in production environment or not
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -52,8 +55,34 @@ app.use(
 const routes = require('./routes');
 app.use(routes);
 
+//middleware to handle cases where a requested resource couldn't be found, and it generates a 404 error.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+})
 
+//middleware to catch Sequelize errors and format them before sending the error response.
+app.use((err, _req, _res, next) => {
+  if(err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+})
 
-
+//middleware to format all errors before returning JSON response
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
