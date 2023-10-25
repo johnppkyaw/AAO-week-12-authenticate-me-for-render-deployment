57c2a0ea10b0f7e462a410ce7ae80dd939f53a69
John <jjpyae@gmail.com>
2023-10-24 22:47:43 -0700

    api session route complete

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res, next) => {
   res.json({
